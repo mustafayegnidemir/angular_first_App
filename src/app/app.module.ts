5653df2d17f14ddaaf4e6ec8fda978a6bf850137
mustafa <mustafayegnidemir1@gmail.com>
2022-02-11 22:00:15 +0300

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { CategoriesComponent } from './admin/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
